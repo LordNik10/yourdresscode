d71713d4e4f803f1d50e82fe6272d9a6cafe5262
LordNik10 <niccolonaso@hotmail.com>
2022-08-12 19:30:43 +0200

    ultimate navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../assests/logo.png';
+import hamburgerMenu from '../assests/hamburger.png';
 import { theme } from '../config/theme';
 import './NavBar.scss';
 
@@ -12,6 +13,25 @@ function NavBar() {
       <div className="container-logo">
         <img src={logo} alt="logo" />
         <span className="website-title">Your Dress Code</span>
+      </div>
+
+      <div className="container-hamburger-menu">
+        <img
+          src={hamburgerMenu}
+          alt="hamburger menu"
+          className="hamburger-icon"
+        />
+        <ul className="menu-list-hamburger">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#products">Products</a>
+          </li>
+          <li>
+            <a href="#contacts">Contacts</a>
+          </li>
+        </ul>
       </div>
 
       <div className="container-menu">
