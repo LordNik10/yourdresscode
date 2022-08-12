d9dacda2e8af5e25c8f9376a295b892425e46354
LordNik10 <niccolonaso@hotmail.com>
2022-08-12 12:42:03 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import "./App.css";
+import NavBar from './components/NavBar';
+import './App.css';
 
 function App() {
   return (
