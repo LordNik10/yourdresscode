89651ffc8c03b2953198c213c4dc5a752d0b5b00
LordNik10 <niccolonaso@hotmail.com>
2022-08-12 17:28:54 +0200

    creation of navbar without material

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,35 @@ import './NavBar.scss';
 
 function NavBar() {
   return (
-    <div
+    <nav
       className="container"
       style={{ backgroundColor: theme.palette.primary.main }}
     >
-      <img src={logo} alt="logo" />
-      <span>Your Dress Code</span>
-    </div>
+      <div className="container-logo">
+        <img src={logo} alt="logo" />
+        <span className="website-title">Your Dress Code</span>
+      </div>
+
+      <div className="container-menu">
+        <ul className="menu-list">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#products">Products</a>
+          </li>
+          <li>
+            <a href="#contacts">Contacts</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="container-login">
+        <span>
+          <a href="#login">Login</a>
+        </span>
+      </div>
+    </nav>
   );
 }
 
