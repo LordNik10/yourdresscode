e456187216b15b19e583982ee9bc3702801b74a6
LordNik10 <niccolonaso@hotmail.com>
2022-08-15 22:45:39 +0200

    wip

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,33 +1,33 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#1D1D1B",
+      main: '#1D1D1B',
     },
     secondary: {
-      main: "#fdc187",
-      contrastText: "#000",
+      main: '#fdc187',
+      contrastText: '#000',
     },
     error: {
-      main: "#d32f2f",
+      main: '#d32f2f',
     },
     warning: {
-      main: "#fbc02d",
+      main: '#fbc02d',
     },
     success: {
-      main: "#388e3c",
+      main: '#388e3c',
     },
     info: {
-      main: "#0288d1",
+      main: '#0288d1',
     },
     bgDark: {
-      main: "#3C3C3C",
-      contrastText: "#fff",
+      main: '#3C3C3C',
+      contrastText: '#fff',
     },
     bgLight: {
-      main: "#fff",
-      contrastText: "#000",
+      main: '#fff',
+      contrastText: '#000',
     },
   },
   breakpoints: {
@@ -47,8 +47,8 @@ export const theme = createTheme({
     MuiTypography: {
       styleOverrides: {
         root: {
-          fontFamily: "Roboto",
-          margin: "4px auto",
+          fontFamily: 'Roboto',
+          margin: '4px auto',
         },
       },
     },
@@ -56,64 +56,64 @@ export const theme = createTheme({
 });
 
 theme.typography.h1 = {
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "28px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '28px',
   },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "40px",
+  [theme.breakpoints.up('md')]: {
+    fontSize: '40px',
   },
 };
 
 theme.typography.h2 = {
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "24px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '24px',
   },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "32px",
+  [theme.breakpoints.up('md')]: {
+    fontSize: '32px',
   },
 };
 
 theme.typography.h3 = {
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "20px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '20px',
   },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "24px",
+  [theme.breakpoints.up('md')]: {
+    fontSize: '24px',
   },
 };
 
 theme.typography.h4 = {
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "18px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '18px',
   },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "20px",
+  [theme.breakpoints.up('md')]: {
+    fontSize: '20px',
   },
 };
 
 theme.typography.h5 = {
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "16px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '16px',
   },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "16px",
+  [theme.breakpoints.up('md')]: {
+    fontSize: '16px',
   },
 };
 
 theme.typography.h6 = {
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "14px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '14px',
   },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "14px",
+  [theme.breakpoints.up('md')]: {
+    fontSize: '14px',
   },
 };
 
 theme.typography.body1 = {
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "16px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '16px',
   },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "16px",
+  [theme.breakpoints.up('md')]: {
+    fontSize: '16px',
   },
 };
