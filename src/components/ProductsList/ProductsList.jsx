2169694e153495cfdf93376827ba4ac9603c5ca4
LordNik10 <niccolonaso@hotmail.com>
2022-09-12 23:18:23 +0200

    wip

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -4,6 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
 
 function MediaCard({ products }) {
   return (
@@ -18,35 +19,36 @@ function MediaCard({ products }) {
       }}
     >
       {products.map((el) => (
-        <Card
-          sx={{
-            width: 350,
-            height: 530,
-            marginBottom: '2%',
-            cursor: 'pointer',
-          }}
-          key={el.id}
-        >
-          <CardMedia
-            component="img"
-            height="400"
-            image={el.image}
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              sx={{ fontSize: '20px' }}
-            >
-              {el.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              ${el.price}
-            </Typography>
-          </CardContent>
-        </Card>
+        <Link to={`/product/${el.id}`} key={el.id}>
+          <Card
+            sx={{
+              width: 350,
+              height: 530,
+              marginBottom: '2%',
+              cursor: 'pointer',
+            }}
+          >
+            <CardMedia
+              component="img"
+              height="400"
+              image={el.image}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                sx={{ fontSize: '20px' }}
+              >
+                {el.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                ${el.price}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Link>
       ))}
     </Container>
   );
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,5 +1,32 @@
+import { Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 function Product() {
-  return <h1>Product</h1>;
+  const [productInfo, setProductInfo] = useState({});
+  const { product } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(`https://fakestoreapi.com/products/${product}`);
+        if (!res.ok) {
+          throw new Error('Error loading product');
+        }
+        const data = await res.json();
+        setProductInfo(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [product]);
+  return (
+    <>
+      <Typography component="h1">{productInfo.title}</Typography>
+      <img src={productInfo.image} alt="product" style={{ width: '30%' }} />
+    </>
+  );
 }
 
 export default Product;
