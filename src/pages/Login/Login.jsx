465ec56165f173ff55a0d16d2159cd12643f0825
LordNik10 <niccolonaso@hotmail.com>
2022-10-02 22:42:22 +0200

    wip

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,19 @@
+import { Typography, Stack, TextField } from '@mui/material';
+
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <Stack
+      flexDirection="row"
+      justifyContent="center"
+      alignItems="center"
+      height="100%"
+    >
+      <Stack flexDirection="column" alignItems="center" width="300px">
+        <Typography component="h1">Login</Typography>
+        <TextField label="Username" variant="outlined" />
+      </Stack>
+    </Stack>
+  );
 }
 
 export default Login;
